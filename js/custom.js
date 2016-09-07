a19219182ea4c9bc0977eb6168972341be5bd946
Saptak Sengupta <saptak013@gmail.com>
2016-09-07 16:58:45 +0530

    Close to fhoke version added

--- js/custom.js
+++ js/custom.js
@@ -187,30 +187,7 @@ function magnificPopup() {
 
 
 
-/* --- Isotope ------------------- */
 
-function isotope() {
-
- var $container = $('#portfolio');
-
- // init
- $container.imagesLoaded( function(){
-   $container.isotope({
-     // options
-     itemSelector: '.portfolio-item',
-     layoutMode: 'fitRows'
-   });
- });
-
- // filter items on button click
- $('#filters').on( 'click', 'button', function( event ) {
-   var filterValue = $(this).attr('data-filter-value');
-   $container.isotope({ filter: filterValue });
-   $('#filters button').removeClass('active');
-   $(this).addClass('active');
- });
-
-}
 
 
 /* --- Scroll to Anchor ------------------- */
