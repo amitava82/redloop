0b9bea46fc6e01223214087acae219ac8fc7fe00
Amitava Saha <amitava82@gmail.com>
2016-09-10 11:22:43 +0530

    style, sidebar

--- js/custom.js
+++ js/custom.js
@@ -9,6 +9,34 @@ $(document).ready(function() {
   fullScreenContainer();
   //owlCarousel();
   //magnificPopup();
+
+  var trigger = $('.hamburger'),
+      overlay = $('.overlay'),
+     isClosed = false;
+
+    trigger.click(function () {
+      hamburger_cross();      
+    });
+
+    function hamburger_cross() {
+
+      if (isClosed == true) {          
+        overlay.hide();
+        trigger.removeClass('is-open');
+        trigger.addClass('is-closed');
+        isClosed = false;
+      } else {   
+        overlay.show();
+        trigger.removeClass('is-closed');
+        trigger.addClass('is-open');
+        isClosed = true;
+      }
+  }
+  
+  $('[data-toggle="offcanvas"]').click(function () {
+        $('#wrapper').toggleClass('toggled');
+  });  
+
 });
 
 
@@ -17,7 +45,6 @@ $(document).ready(function() {
 
 function init() {
   //tooltips();
-  onePageScroll();
   scrollAnchor();
   toggleContactForm();
 }
@@ -88,80 +115,6 @@ window.scrollReveal = new scrollReveal();
 
 /* --- magnific popup ------------------- */
 
-function magnificPopup() {
-
-  // Gallery
-  $('.popup-gallery').magnificPopup({
-    type: 'image',
-    tLoading: 'Loading image #%curr%...',
-    mainClass: 'mfp-fade',
-    disableOn: 700,
-    removalDelay: 160,
-    gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-    },
-    image: {
-      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-    },
-    callbacks: {
-      close: function() {
-        $('.portfolio-item figure figcaption').removeClass('active');
-        $('.portfolio-item figure .info').removeClass('active');
-      }
-    }
-  });
-
-  $('.portfolio-item figcaption a.preview').click(function(){
-    $(this).parent().addClass('active');
-    $(this).parent().siblings('.info').addClass('active');
-  });
-
-  // Zoom Gallery
-
-  $('.zoom-modal').magnificPopup({
-    type: 'image',
-    mainClass: 'mfp-with-zoom', // this class is for CSS animation below
-
-    zoom: {
-      enabled: true, // By default it's false, so don't forget to enable it
-
-      duration: 300, // duration of the effect, in milliseconds
-      easing: 'ease-in-out', // CSS transition easing function
-
-      // The "opener" function should return the element from which popup will be zoomed in
-      // and to which popup will be scaled down
-      // By defailt it looks for an image tag:
-      opener: function(openerElement) {
-        // openerElement is the element on which popup was initialized, in this case its <a> tag
-        // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-        return openerElement.is('i') ? openerElement : openerElement.find('i');
-      }
-    }
-
-  });
-
-  $('.popup-modal').magnificPopup({
-		type: 'inline',
-
-		fixedContentPos: false,
-		fixedBgPos: true,
-
-		overflowY: 'auto',
-
-		closeBtnInside: true,
-		preloader: false,
-
-		midClick: true,
-		removalDelay: 300,
-		mainClass: 'my-mfp-slide-bottom'
-	});
-}
-
-
-
-
 
 
 /* --- Scroll to Anchor ------------------- */
@@ -184,133 +137,8 @@ function scrollAnchor() {
 
 }
 
-
-/* --- Modal overlay (used for signup form) ------------------- */
-
-function signupOverlay() {
-  var container = document.querySelector( 'div.container' ),
-    triggerBttn = document.getElementsByClassName( 'signup' ),
-    overlay = document.querySelector( 'div#signup' ),
-    closeBttn = overlay.querySelector( 'button.overlay-close' );
-    transEndEventNames = {
-      'WebkitTransition': 'webkitTransitionEnd',
-      'MozTransition': 'transitionend',
-      'OTransition': 'oTransitionEnd',
-      'msTransition': 'MSTransitionEnd',
-      'transition': 'transitionend'
-    },
-    transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
-    support = { transitions : Modernizr.csstransitions };
-
-  function toggleOverlay() {
-    if( classie.has( overlay, 'open' ) ) {
-      classie.remove( overlay, 'open' );
-      classie.remove( container, 'overlay-open' );
-      classie.add( overlay, 'close-me' );
-      var onEndTransitionFn = function( ev ) {
-        if( support.transitions ) {
-          if( ev.propertyName !== 'visibility' ) return;
-          this.removeEventListener( transEndEventName, onEndTransitionFn );
-        }
-        classie.remove( overlay, 'close-me' );
-      };
-      if( support.transitions ) {
-        overlay.addEventListener( transEndEventName, onEndTransitionFn );
-      }
-      else {
-        onEndTransitionFn();
-      }
-    }
-    else if( !classie.has( overlay, 'close-me' ) ) {
-      classie.add( overlay, 'open' );
-      classie.add( container, 'overlay-open' );
-    }
-  }
-
-  for (i = 0; i < triggerBttn.length; i++) {
-      triggerBttn[i].addEventListener( 'click', toggleOverlay );
-  }
-  closeBttn.addEventListener( 'click', toggleOverlay );
-
-  $('.signup').click(function(e) {
-      e.preventDefault();
-  });
-}
-
-/* --- Modal overlay (used for login form) ------------------- */
-
-function loginOverlay() {
-  var container = document.querySelector( 'div.container' ),
-    triggerBttn = document.querySelector( '.login' ),
-    overlay = document.querySelector( 'div#login' ),
-    closeBttn = overlay.querySelector( 'button.overlay-close' );
-    transEndEventNames = {
-      'WebkitTransition': 'webkitTransitionEnd',
-      'MozTransition': 'transitionend',
-      'OTransition': 'oTransitionEnd',
-      'msTransition': 'MSTransitionEnd',
-      'transition': 'transitionend'
-    },
-    transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
-    support = { transitions : Modernizr.csstransitions };
-
-  function toggleOverlay() {
-    if( classie.has( overlay, 'open' ) ) {
-      classie.remove( overlay, 'open' );
-      classie.remove( container, 'overlay-open' );
-      classie.add( overlay, 'close-me' );
-      var onEndTransitionFn = function( ev ) {
-        if( support.transitions ) {
-          if( ev.propertyName !== 'visibility' ) return;
-          this.removeEventListener( transEndEventName, onEndTransitionFn );
-        }
-        classie.remove( overlay, 'close-me' );
-      };
-      if( support.transitions ) {
-        overlay.addEventListener( transEndEventName, onEndTransitionFn );
-      }
-      else {
-        onEndTransitionFn();
-      }
-    }
-    else if( !classie.has( overlay, 'close-me' ) ) {
-      classie.add( overlay, 'open' );
-      classie.add( container, 'overlay-open' );
-    }
-  }
-
-  triggerBttn.addEventListener( 'click', toggleOverlay );
-  closeBttn.addEventListener( 'click', toggleOverlay );
-
-  $('.login').click(function(e) {
-      e.preventDefault();
-  });
-}
-
-
-
 /* --- One Page Scroll ------------------- */
 
-function onePageScroll() {
-  $('.nav').onePageNav({
-      currentClass: 'current',
-      changeHash: false,
-      scrollSpeed: 650,
-      scrollOffset: 30,
-      scrollThreshold: 0.5,
-      filter: ':not(.login, .signup)',
-      easing: 'swing',
-      begin: function() {
-          //I get fired when the animation is starting
-      },
-      end: function() {
-          //I get fired when the animation is ending
-      },
-      scrollChange: function($currentListItem) {
-          //I get fired when you enter a section and I pass the list item of the section
-      }
-  });
-}
 
 
 $(window).scroll(function() {
