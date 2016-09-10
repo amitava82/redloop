5ced71d7332b4de59e52f707cf74b7b93b51f992
Amitava Saha <amitava82@gmail.com>
2016-09-10 13:50:19 +0530

    more style and email integration

--- js/custom.js
+++ js/custom.js
@@ -1,87 +1,131 @@
 /* -- Full Screen Viewport Container
-   ---------------------------- */
+ ---------------------------- */
+fullScreenContainer();
 
-$(window).load(function(){
+$(document).ready(function () {
+
+    //owlCarousel();
+    //magnificPopup();
     init();
-});
 
-$(document).ready(function() {
-  fullScreenContainer();
-  //owlCarousel();
-  //magnificPopup();
-
-  var trigger = $('.hamburger'),
-      overlay = $('.overlay'),
-     isClosed = false;
+    var trigger = $('.hamburger'),
+        overlay = $('.overlay'),
+        isClosed = false;
 
     trigger.click(function () {
-      hamburger_cross();      
+        hamburger_cross();
     });
 
     function hamburger_cross() {
 
-      if (isClosed == true) {          
-        overlay.hide();
-        trigger.removeClass('is-open');
-        trigger.addClass('is-closed');
-        isClosed = false;
-      } else {   
-        overlay.show();
-        trigger.removeClass('is-closed');
-        trigger.addClass('is-open');
-        isClosed = true;
-      }
-  }
-  
-  $('[data-toggle="offcanvas"]').click(function () {
+        if (isClosed == true) {
+            overlay.hide();
+            trigger.removeClass('is-open');
+            trigger.addClass('is-closed');
+            isClosed = false;
+        } else {
+            overlay.show();
+            trigger.removeClass('is-closed');
+            trigger.addClass('is-open');
+            isClosed = true;
+        }
+    }
+
+    $('[data-toggle="offcanvas"]').click(function () {
         $('#wrapper').toggleClass('toggled');
-  });  
+    });
+
+    $('#contactForm').on('submit', function (e) {
+        e.preventDefault();
+        var name = $("input#name").val();
+        var phone = $("input#phone").val();
+        var email = $("input#email").val();
+        var message = $("textarea#message").val();
+
+        var $btn = $('button', '.submit-button');
+
+        $btn.attr('disabled', true);
+
+        sendMail({
+            name: name,
+            phone: phone,
+            email: email,
+            message: message
+        }).then(function (response) {
+            console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
+            // Success message
+            $('#success').html("<div class='alert alert-success'>");
+            $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                .append("</button>");
+            $('#success > .alert-success')
+                .append("<strong>Your message has been sent. </strong>");
+            $('#success > .alert-success')
+                .append('</div>');
+
+            //clear all fields
+            $('#contactForm').trigger("reset");
+            $btn.attr('disabled', false);
+        }, function (err) {
+            console.log("FAILED. error=", err);
+            // Fail message
+            $('#success').html("<div class='alert alert-danger'>");
+            $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                .append("</button>");
+            $('#success > .alert-danger').append("<strong>Sorry " + name + " it seems that my mail server is not responding...</strong>");
+            $('#success > .alert-danger').append('</div>');
+            //clear all fields
+            $('#contactForm').trigger("reset");
+            $btn.attr('disabled', false)
+        });
+
+    })
 
 });
-
 
 
 /* --- initialize functions on window load here -------------- */
 
 function init() {
-  //tooltips();
-  scrollAnchor();
-  toggleContactForm();
+    //tooltips();
+    scrollAnchor();
+    toggleContactForm();
 }
 
-
+function sendMail(data) {
+    return emailjs.send("gmail", "template_78ZXkLdA", data);
+}
 
 /* --- Full Screen Container ------------- */
 
 function fullScreenContainer() {
 
-  // Set Initial Screen Dimensions
-
-  var screenWidth = $(window).width() + "px";
-  var screenHeight = $(window).height() + "px";
-
-  $("#intro, #intro .item, #intro-video").css({
-    width: screenWidth,
-    height: screenHeight
-  });
-
-  // Every time the window is resized...
-
-  $(window).resize( function () {
-
-    // Fetch Screen Dimensions
+    // Set Initial Screen Dimensions
 
     var screenWidth = $(window).width() + "px";
     var screenHeight = $(window).height() + "px";
 
-    // Set Slides to new Screen Dimensions
-
-    $("#intro, #intro .item, #intro-video, #intro-video .item").css({
-      width: screenWidth,
-      height: screenHeight
+    $("#intro, #intro .item, #intro-video").css({
+        width: screenWidth,
+        height: screenHeight
     });
 
-  });
+    // Every time the window is resized...
+
+    $(window).resize(function () {
+
+        // Fetch Screen Dimensions
+
+        var screenWidth = $(window).width() + "px";
+        var screenHeight = $(window).height() + "px";
+
+        // Set Slides to new Screen Dimensions
+
+        $("#intro, #intro .item, #intro-video, #intro-video .item").css({
+            width: screenWidth,
+            height: screenHeight
+        });
+
+    });
 
 }
 
@@ -89,28 +133,23 @@ function fullScreenContainer() {
 /* --- Tooltips ------------------- */
 
 function tooltips() {
-  $('.tooltips').tooltip();
+    $('.tooltips').tooltip();
 }
-
-
 
 
 /* --- Show/Hide Contact Form ------------------- */
 
 function toggleContactForm() {
-  $('.contact-button').click(function() {
-    $(this).toggleClass('active');
-    $('.contact-form').slideToggle(300);
-  });
+    $('.contact-button').click(function () {
+        $(this).toggleClass('active');
+        $('.contact-form').slideToggle(300);
+    });
 }
-
-
 
 
 /* --- scrollReveal ------------------- */
 
 window.scrollReveal = new scrollReveal();
-
 
 
 /* --- magnific popup ------------------- */
@@ -121,139 +160,68 @@ window.scrollReveal = new scrollReveal();
 
 function scrollAnchor() {
 
-  // scroll to specific anchor
-  $('.scroll').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 650);
-        return false;
-      }
-    }
-  });
+    // scroll to specific anchor
+    $('.scroll').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 650);
+                return false;
+            }
+        }
+    });
 
 }
 
 /* --- One Page Scroll ------------------- */
 
 
+$(window).scroll(function () {
+    var windowpos = $(window).scrollTop();
 
-$(window).scroll(function() {
-  var windowpos = $(window).scrollTop() ;
-
-  if (windowpos <= 500) {
-      $('.nav li.current').removeClass('current');
-  }
+    if (windowpos <= 500) {
+        $('.nav li.current').removeClass('current');
+    }
 });
 
 
-
-
 //Placeholder fixed for Internet Explorer
-$(function() {
-	var input = document.createElement("input");
-	if(('placeholder' in input)==false) {
-		$('[placeholder]').focus(function() {
-			var i = $(this);
-			if(i.val() == i.attr('placeholder')) {
-				i.val('').removeClass('placeholder');
-				if(i.hasClass('password')) {
-					i.removeClass('password');
-					this.type='password';
-				}
-			}
-		}).blur(function() {
-			var i = $(this);
-			if(i.val() == '' || i.val() == i.attr('placeholder')) {
-				if(this.type=='password') {
-					i.addClass('password');
-					this.type='text';
-				}
-				i.addClass('placeholder').val(i.attr('placeholder'));
-			}
-		}).blur().parents('form').submit(function() {
-			$(this).find('[placeholder]').each(function() {
-				var i = $(this);
-				if(i.val() == i.attr('placeholder'))
-					i.val('');
-			})
-		});
-	}
-	});
-
-
-
-/*
-  Jquery Validation using jqBootstrapValidation
-   example is taken from jqBootstrapValidation docs
-  */
-$(function() {
-
- $("input,textarea").jqBootstrapValidation(
-    {
-     preventSubmit: true,
-     submitError: function($form, event, errors) {
-      // something to have when submit produces an error ?
-      // Not decided if I need it yet
-     },
-     submitSuccess: function($form, event) {
-      event.preventDefault(); // prevent default submit behaviour
-       // get values from FORM
-       var first_name = $("input#first_name").val();
-       var last_name = $("input#last_name").val();
-       var email = $("input#email").val();
-       var message = $("textarea#message").val();
-        var firstName = name; // For Success/Failure Message
-           // Check for white space in name for Success/Fail message
-        if (firstName.indexOf(' ') >= 0) {
-	   firstName = name.split(' ').slice(0, -1).join(' ');
-         }
-	   $.ajax({
-                url: "contact_me.php",
-            	type: "POST",
-            	data: {first_name: first_name, last_name: last_name, email: email, message: message},
-            	cache: false,
-            	success: function() {
-            	// Success message
-            	   $('#success').html("<div class='alert alert-success'>");
-            	   $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            		.append( "</button>");
-            	  $('#success > .alert-success')
-            		.append("<strong>Your message has been sent. </strong>");
- 		  $('#success > .alert-success')
- 			.append('</div>');
-
- 		  //clear all fields
- 		  $('#contactForm').trigger("reset");
- 	      },
- 	   error: function() {
- 		// Fail message
- 		 $('#success').html("<div class='alert alert-danger'>");
-            	$('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            	 .append( "</button>");
-            	$('#success > .alert-danger').append("<strong>Sorry "+firstName+" it seems that my mail server is not responding...</strong> Could you please email me directly to <a href='mailto:me@example.com?Subject=Message_Me;>me@example.com</a> ? Sorry for the inconvenience!");
- 	        $('#success > .alert-danger').append('</div>');
- 		//clear all fields
- 		$('#contactForm').trigger("reset");
- 	    },
-           })
-         },
-         filter: function() {
-                   return $(this).is(":visible");
-         },
-       });
-
-      $("a[data-toggle=\"tab\"]").click(function(e) {
-                    e.preventDefault();
-                    $(this).tab("show");
+$(function () {
+    var input = document.createElement("input");
+    if (('placeholder' in input) == false) {
+        $('[placeholder]').focus(function () {
+            var i = $(this);
+            if (i.val() == i.attr('placeholder')) {
+                i.val('').removeClass('placeholder');
+                if (i.hasClass('password')) {
+                    i.removeClass('password');
+                    this.type = 'password';
+                }
+            }
+        }).blur(function () {
+            var i = $(this);
+            if (i.val() == '' || i.val() == i.attr('placeholder')) {
+                if (this.type == 'password') {
+                    i.addClass('password');
+                    this.type = 'text';
+                }
+                i.addClass('placeholder').val(i.attr('placeholder'));
+            }
+        }).blur().parents('form').submit(function () {
+            $(this).find('[placeholder]').each(function () {
+                var i = $(this);
+                if (i.val() == i.attr('placeholder'))
+                    i.val('');
+            })
         });
-  });
+    }
+});
 
 
 /*When clicking on Full hide fail/success boxes */
-$('#name').focus(function() {
-     $('#success').html('');
-  });
+$('#name').focus(function () {
+    $('#success').html('');
+});
